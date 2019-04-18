3131cda0016fbc438daee59517437a1bdd39c7d5
1ba1 <vvvalenti01@yahoo.it>
2019-04-18 12:51:55 +0200

    implementing log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user
+      log_in @user
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to login_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  #get 'users/new'
-  #get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
+  get '/signup', to: 'users#new'
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
