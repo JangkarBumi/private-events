68d471c400817345096736790992c19812cbe34c
Zeha Irawan <jangkaringbumi@gmail.com>
2019-04-18 20:38:06 +0700

    add display event functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -5,15 +7,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if logged_in?
+    @events = @user.events 
+    else
+      redirect_to login_path
+    end
   end
 
   def create
     @user = User.new(username: params[:user][:username])
-    if (@user.save)
+    if @user.save
       redirect_to user_path(@user)
     else
       render 'new'
     end
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_134620) do
+ActiveRecord::Schema.define(version: 2019_04_18_125826) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "info"
+    t.string "location"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
